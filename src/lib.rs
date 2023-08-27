f9c7d949397d429b24134b9c81da708855abf3b1
Murtaza Imtiaz <self@murtazai.org>
2023-08-27 00:00:50 -0400

    Initiation

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,15 @@
+/// Principal
+pub struct Principal {
+    pub is_alive: bool,
+    pub net_cash_flow_at_time_t: u128, 
+    pub time_of_cash_flow: u128, 
+    pub discount_rate: u128
+} 
+
+
 /// Calculate net present value
-pub fn net_present_value(net_cash_flow_at_time_t: u128, time_of_cash_flow: u128, discount_rate: u128) -> u128 {
-    net_cash_flow_at_time_t / (1 + discount_rate).pow(time_of_cash_flow as u32)
+pub fn net_present_value(principal: Principal) -> u128 {
+    principal.net_cash_flow_at_time_t / (1 + principal.discount_rate).pow(principal.time_of_cash_flow as u32)
 }
 
 /// Check if principal is a deponent
@@ -18,12 +27,24 @@ pub fn is_deponent(deponent_net_present_value: u128) -> bool {
 mod tests {
     use super::*;
 
+    fn init_principal() -> Principal {
+        Principal {
+            is_alive: true,
+            net_cash_flow_at_time_t: u128::MAX, 
+            time_of_cash_flow: u128::MAX, 
+            discount_rate: u128::MAX,
+        }
+    }
+
     /// Verify '{net_present_value}' panic.
     #[test]
     #[should_panic]
     fn verify_net_present_value() {
-        let result = net_present_value(u128::MAX, u128::MAX, u128::MAX);
-        assert_eq!(result, u128::MAX);
+        let principal = init_principal();
+
+
+        let current_net_present_value = net_present_value(principal);
+        assert_eq!(current_net_present_value, u128::MAX);
     }
 
     /// Verify a principal as not a deponent
