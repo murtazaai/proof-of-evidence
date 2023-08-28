58764758876d908e9a4184514213e32fd81c5566
Murtaza Imtiaz <self@murtazai.org>
2023-08-28 01:21:11 -0400

    Amendments

--- src/lib.rs
+++ src/lib.rs
@@ -1,30 +1,46 @@
+/// Holder
+pub trait Holder {
+    /// Net present value
+    fn net_present_value(account: Account) -> u128;
+
+    /// Is '{Principal}' a deponent
+    fn is_deponent(account: Account) -> bool;
+}
+
 /// Principal
 pub struct Principal {
     pub is_alive: bool,
+}
+
+/// Wallet
+pub struct Wallet {
     pub net_cash_flow_at_time_t: u128, 
     pub time_of_cash_flow: u128, 
     pub discount_rate: u128
-} 
-
-
-/// Calculate net present value
-pub fn net_present_value(principal: Principal) -> u128 {
-    if principal.is_alive {
-        principal.net_cash_flow_at_time_t / (1 + principal.discount_rate).pow(principal.time_of_cash_flow as u32)
-    } else {
-        0
-    }
-    
 }
 
-/// Check if principal is a deponent
-pub fn is_deponent(is_alive: bool, deponent_net_present_value: u128) -> bool {
-    if is_alive && deponent_net_present_value > u64::MAX  as u128  {
-        true
-    } else {
-        false
+pub struct Account {
+    pub principal: Principal,
+    pub wallet: Wallet,
+}
+
+/// Principal implementation
+impl Holder for Account {
+    fn net_present_value(account: Account) -> u128 {
+        if account.principal.is_alive {
+            account.wallet.net_cash_flow_at_time_t / (1 + account.wallet.discount_rate).pow(account.wallet.time_of_cash_flow as u32)
+        } else {
+            0
+        }
     }
 
+    fn is_deponent(account: Account) -> bool {
+        if account.principal.is_alive && Self::net_present_value(account) > u64::MAX  as u128  {
+            true
+        } else {
+            false
+        }    
+    }
 }
 
 /// Unit test cases
@@ -32,35 +48,41 @@ pub fn is_deponent(is_alive: bool, deponent_net_present_value: u128) -> bool {
 mod tests {
     use super::*;
 
-    fn init_principal() -> Principal {
-        Principal {
-            is_alive: true,
-            net_cash_flow_at_time_t: u128::MAX, 
-            time_of_cash_flow: u128::MAX, 
-            discount_rate: u128::MAX,
+    fn init_account() -> Account {
+        Account {
+            principal: Principal { 
+                is_alive: true
+            },
+            wallet: Wallet {
+                net_cash_flow_at_time_t: 99999 as u128, 
+                time_of_cash_flow: 3 as u128, 
+                discount_rate: 9 as u128,
+            },
         }
     }
 
     /// Verify '{net_present_value}' panic.
     #[test]
-    #[should_panic]
     fn verify_net_present_value() {
-        let principal = init_principal();
+        let account = init_account();
 
-
-        let current_net_present_value = net_present_value(principal);
-        assert_eq!(current_net_present_value, u128::MAX);
+        let current_net_present_value = Account::net_present_value(account);
+        assert_eq!(current_net_present_value, 99);
     }
 
     /// Verify a principal as not a deponent
     #[test]
     fn verify_principal_is_not_a_deponent() {
-        assert_eq!(is_deponent(true, u128::MIN), false); 
+        let account = init_account();
+
+        assert_eq!(Account::is_deponent(account), false); 
     }
 
     /// Verify principal as a deponent
     #[test]
     fn verify_principal_is_a_deponent() {
-        assert_eq!(is_deponent(true, u64::MAX as u128), false); 
+        let account = init_account();
+
+        assert_eq!(Account::is_deponent(account), false); 
     }
 }
